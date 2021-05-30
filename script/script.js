9611440e2faf063bd244f68d79882d7984982d2b
ValeriaDutka <85069192+Valeria808ll@users.noreply.github.com>
2021-05-30 18:24:40 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$('.slider').slick({
+    
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1
+    
+  });
+          
+
+  
+$('.fade').slick({
+    
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
